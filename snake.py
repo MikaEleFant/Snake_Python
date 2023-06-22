acf833ee533f2318513aa7e7722896930e45c133
MikaEleFant <changholee2wpes@gmail.com>
2023-06-22 13:43:17 -0700

    Implement Class snake with snake methods

--- snake.py
+++ snake.py
@@ -8,3 +8,30 @@ class Snake:
                     [70, 50]]
         self.direction = "RIGHT"
         self.turn = self.direction
+
+    def move(self, direction):
+        self.turn = direction
+
+        if self.turn == "UP" and self.direction != "DOWN":
+            self.direction = "UP"
+        if self.turn == "DOWN" and self.direction != "UP":
+            self.direction = "DOWN"
+        if self.turn == "LEFT" and self.direction != "RIGHT":
+            self.direction = "LEFT"
+        if self.turn == "RIGHT" and self.direction != "LEFT":
+            self.direction = "RIGHT"
+
+        if self.direction == "UP":
+            self.pos[0] -= 10
+        if self.direction == "DOWN":
+            self.pos[0] += 10
+        if self.direction == "LEFT":
+            self.pos[1] -= 10
+        if self.direction == "RIGHT":
+            self.pos[1] += 10
+
+        self.body.insert(0, list(self.pos))
+        self.body.pop()
+
+    def grow(self):
+        self.body.insert(0, list(self.pos))
