09bc2726c046d94ea85de66a736a55c48a748267
MikaEleFant <changholee2wpes@gmail.com>
2023-06-22 11:23:37 -0700

    Declare & assign variables needed for snake

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ import pygame
 import time
 import random
 
-# 5 27 30 35
+# Variables
+
+snake_speed = 15
+
+window_width = 720
+window_height = 480
+
+BLACK = pygame.Color(0, 0, 0)
+WHITE = pygame.Color(255, 255, 255)
+RED = pygame.Color(255, 0, 0)
+GREEN = pygame.Color(0, 255, 0)
+BLUE = pygame.Color(0, 0, 255)
